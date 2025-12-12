048165422f0556e71f59942fa80ba4ac60b52c56
Andrew Mos <andresmosquera739@gmail.com>
2025-12-12 14:35:55 -0500

    feat: set sidebar to collapsed by default on dashboard

--- src/app/[locale]/(auth)/dashboard/page.tsx
+++ src/app/[locale]/(auth)/dashboard/page.tsx
@@ -51,37 +51,37 @@ export default async function Page() {
 
   return (
     <RestaurantProvider defaultRestaurant={primerRestaurante}>
-      <SidebarProvider>
+    <SidebarProvider defaultOpen={false}>
         <AppSidebar restaurantes={restaurantes} />
-        <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
                     Dashboard
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
                   <BreadcrumbPage>Mesas y Domicilios</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </header>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
           <DashboardContent
             todasLasMesas={mesas}
             todosLosDomicilios={domicilios}
             mesasConCarrito={mesasConCarrito}
             domiciliosConCarrito={domiciliosConCarrito}
           />
-        </SidebarInset>
-      </SidebarProvider>
+      </SidebarInset>
+    </SidebarProvider>
     </RestaurantProvider>
   );
 }
